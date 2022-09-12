30b5a44b5cfc8bc1366477add7c638e3eab37d56
AlyaSetya <113299534+AlyaSetya@users.noreply.github.com>
2022-09-12 23:59:09 +0700

    Add files via upload

---
+++ Tugas1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "-----------------------------------"
+echo "Tugas 1 SISOP Kelas-A"
+echo "-----------------------------------"
+echo "Nama : Alya Setya Paramita"
+echo "NPM  : 21083010046"
+echo "-----------------------------------"
+echo "notes"
+echo "-----------------------------------"
+echo "Halo Dunia!"
+echo "Ini adalah skrip Bash pertamaku di Linux"
