2262414ab5965680b9c6fa3518b21172aad3f13d
AlyaSetya <113299534+AlyaSetya@users.noreply.github.com>
2022-09-18 23:40:01 +0700

    Add files via upload

---
+++ percabangan1.sh
@@ -0,0 +1,16 @@
+a=15
+b=7
+
+if [ $a == $b ]
+then
+ echo "a sama dengan b"
+elif [ $a -gt $b]
+then
+ echo "a lebih besar dari b"
+elif [ $a -lt $b ]
+then 
+ echo " lebih kecil dari b"
+else
+ echo "Tidak ada kondisi yang memenuhi"
+fi
+
