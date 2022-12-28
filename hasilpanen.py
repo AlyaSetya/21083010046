7b080c5cdbb71c1e11290aa3f69c1aaa25d56086
AlyaSetya <113299534+AlyaSetya@users.noreply.github.com>
2022-12-28 22:19:57 +0700

    Add files via upload

---
+++ hasilpanen.py
@@ -0,0 +1,249 @@
+print (' _______________________________________ ')
+print ('|                                       |')
+print ('|          ! SUKSES BERKEBUN !          |')
+print ('|_______________________________________|')
+print (' _______________________________________ ')
+print ('|                                       |')
+print ('|      BAGAIMANA KABAR BERKEBUNMU       |')
+print ('|---------------------------------------|')
+print ('|  Menghitung Hasil Kebun yang Akurat   |')
+print ('|_______________________________________|')
+
+def pilihan_kebun():
+    print (" _______________________________________ ")
+    print ("|                                       |")
+    print ("|     Mulai Menghitung Hasil Kebun      |")
+    print ("|---------------------------------------|")
+    print ("|              1. Kangkung              |")
+    print ("|              2. Bayam                 |")
+    print ("|              3. Tomat                 |")
+    print ("|              4. Cabai                 |")
+    print ("|              5. Selada                |")
+    print ("|              6. Daun Bawang           |")
+    print ("|              7. Seledri               |")
+    print ("|              8. Katuk                 |")
+    print ("|              9. Custom                |")
+    print ("|_______________________________________|")
+
+    pilihan=int(input("Pilih Kebunmu : "))
+
+    if pilihan == 1:
+       print ("Hitung Kebun Kangkung")
+       target=int(input("Target Berkebun (kg) : "))
+       hasil=int(input("Hasil Berkebun (kg)  : "))
+
+       if (target<hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target==hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target>hasil):
+           print ("----------------------------------------")
+           print ("Berkebunmu Belum Berhasil Ayo Coba Lagi")
+           print ("----------------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("----------------------------------------")
+
+    elif pilihan == 2:
+       print ("Hitung Kebun Bayam")
+       target=int(input("Target Berkebun (kg) : "))
+       hasil=int(input("Hasil Berkebun (kg)  : "))
+
+       if (target<hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target==hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target>hasil):
+           print ("----------------------------------------")
+           print ("Berkebunmu Belum Berhasil Ayo Coba Lagi")
+           print ("----------------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("----------------------------------------")
+
+    elif pilihan == 3:
+       print ("Hitung Kebun Tomat")
+       target=int(input("Target Berkebun (kg) : "))
+       hasil=int(input("Hasil Berkebun (kg)  : "))
+
+       if (target<hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target==hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target>hasil):
+           print ("----------------------------------------")
+           print ("Berkebunmu Belum Berhasil Ayo Coba Lagi")
+           print ("----------------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("----------------------------------------")
+
+    elif pilihan == 4:
+       print ("Hitung Kebun Cabai")
+       target=int(input("Target Berkebun (kg) : "))
+       hasil=int(input("Hasil Berkebun (kg)  : "))
+
+       if (target<hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target==hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target>hasil):
+           print ("----------------------------------------")
+           print ("Berkebunmu Belum Berhasil Ayo Coba Lagi")
+           print ("----------------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("----------------------------------------")
+
+    elif pilihan == 5:
+       print ("Hitung Kebun Selada")
+       target=int(input("Target Berkebun (kg) : "))
+       hasil=int(input("Hasil Berkebun (kg)  : "))
+
+       if (target<hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target==hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target>hasil):
+           print ("----------------------------------------")
+           print ("Berkebunmu Belum Berhasil Ayo Coba Lagi")
+           print ("----------------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("----------------------------------------")
+
+    elif pilihan == 6:
+       print ("Hitung Kebun Daun Bawang")
+       target=int(input("Target Berkebun (kg) : "))
+       hasil=int(input("Hasil Berkebun (kg)  : "))
+
+       if (target<hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target==hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target>hasil):
+           print ("----------------------------------------")
+           print ("Berkebunmu Belum Berhasil Ayo Coba Lagi")
+           print ("----------------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("----------------------------------------")
+
+    elif pilihan == 7:
+       print ("Hitung Kebun Seledri")
+       target=int(input("Target Berkebun (kg) : "))
+       hasil=int(input("Hasil Berkebun (kg)  : "))
+
+       if (target<hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target==hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target>hasil):
+           print ("----------------------------------------")
+           print ("Berkebunmu Belum Berhasil Ayo Coba Lagi")
+           print ("----------------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("----------------------------------------")
+
+    elif pilihan == 8:
+       print ("Hitung Kebun Katuk")
+       target=int(input("Target Berkebun (kg) : "))
+       hasil=int(input("Hasil Berkebun (kg)  : "))
+
+       if (target<hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target==hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target>hasil):
+           print ("----------------------------------------")
+           print ("Berkebunmu Belum Berhasil Ayo Coba Lagi")
+           print ("----------------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("----------------------------------------")
+
+    elif pilihan == 9:
+       str(input("Pilih Kebunmu Sendiri : "))
+       target=int(input("Target Berkebun (kg) : "))
+       hasil=int(input("Hasil Berkebun (kg)  : "))
+
+       if (target<hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target==hasil):
+           print ("------------------------------")
+           print ("Berkebunmu Sukses")
+           print ("------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("------------------------------")
+       elif (target>hasil):
+           print ("----------------------------------------")
+           print ("Berkebunmu Belum Berhasil Ayo Coba Lagi")
+           print ("----------------------------------------")
+           print ("SEMANGAT BERKEBUN")
+           print ("----------------------------------------")
+
+    else:
+        print("Kebun Tidak Tersedia")
+
+pilihan_kebun()
